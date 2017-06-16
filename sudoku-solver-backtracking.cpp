24581edd052b1767864138d5b6fc11a989124a37
Savita Seetaraman <savita.seetaraman5@gmail.com>
2017-06-16 13:46:17 +0530

    Added corrections

--- sudoku-solver-backtracking.cpp
+++ sudoku-solver-backtracking.cpp
@@ -1,12 +1,39 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std; 
+
 #include <iostream>
 
 using namespace std; 
+
+#define N 9
 
 bool notassigned(int x) {
     return (x == 0)? true : false;
 }
 
-bool usedInRow(int grid[][], int row, int digit) {
+bool usedInRow(int grid[N][N], int row, int digit) {
     for (int i = 0; i < N; i++)
         if (grid[row][i] == digit)
             return true;
@@ -14,45 +41,39 @@ bool usedInRow(int grid[][], int row, int digit) {
     return false;
 }
 
-bool usedInColumn(int grid[][], int column, int digit) {
-    for (int j = 0; j < M; j++) 
+bool usedInColumn(int grid[N][N], int column, int digit) {
+    for (int j = 0; j < N; j++) 
         if (grid[j][column] == digit)
             return true;
 
     return false;
 }
 
-bool usedInGrid(int grid[][], int row, int column, int digit) {
+bool usedInGrid(int grid[N][N], int row, int column, int digit) {
     /* Rounding up to get the end of grid from cell position*/
     int g_r_start = (row / 3) * 3;
-    int g_r_end = (row + 3 - 1) / 3;
+    int g_r_end = (row == 0)? 0 : ((row + 3 - 1) / 3) * 3;
     int g_c_start = (column / 3) * 3;
-    int g_c_end = (column + 3 - 1) / 3;
+    int g_c_end = (column == 0)? 0: ((column + 3 - 1) / 3) * 3;
     
-    for (int i = g_r_start; i <= g_r_end; i++) 
-        for (int j = g_c_start; j <= g_c_end; j++) 
+    for (int i = g_r_start; i < g_r_end; i++) 
+        for (int j = g_c_start; j < g_c_end; j++) 
             if (grid[i][j] == digit) 
                 return true; 
 
     return false; 
 }
 
-int foundCellNotAssigned (int grid[][]) {
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            if (grid[i][j] == 0) 
-                return true;
-        }
-    }
-}
 
-bool solve(int grid[][]) {
+
+bool solve(int grid[N][N]) {
     
     int row = -1, column = -1;
-    for (int i = 0; i <= 9; i++) {
-        bool flag = false;
-        for (int j = 0; j <= 9; j++) {
-            if (grid[i][j] == UNASSIGNED) {
+    bool flag = false;
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (grid[i][j] == 0) {
+                row = i, column = j;
                 flag = true;
                 break;
             }
@@ -61,21 +82,52 @@ bool solve(int grid[][]) {
             break;
     }
 
-    if (flag == false)
+    if (row == -1 && column == -1)
         return true; 
 
     for (int k = 1; k <= 9; k++) {
 
-        if ( !usedInRow(grid, i, k) && !usedInColumn(grid, j, k) && !usedInGrid(grid, i, j, k)) {
-
-            grid[i][j] = k; 
+        if ( !usedInRow(grid, row, k) && !usedInColumn(grid, column, k) && 
+            !usedInGrid(grid, row, column, k) ) {
+            grid[row][column] = k; 
         
             if (solve(grid))
                 return true;
        
-            grid[i][j] == 0;
+            grid[row][column] = 0;
         }
     }           
     
     return false;
+}
+
+int printSolution(int grid[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << grid[i][j] << " ";
+        }
+    }
+}
+
+int main() {
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; i++) {
+        int grid[N][N];
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < N; k++) {
+                cin>>grid[j][k];
+            }
+        }
+        if (solve(grid) == true)
+            printSolution(grid);
+        else {
+            printSolution(grid);
+            cout << "Not solveable";
+        }
+        
+        cout << endl;
+    }
+	
+	return 0;
 }

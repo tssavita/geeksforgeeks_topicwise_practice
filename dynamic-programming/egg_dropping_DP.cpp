f62f2675648f7ae8399ab07e18941ee8b0d8ca59
Savita Seetaraman <savita.seetaraman5@gmail.com>
2018-03-11 17:20:00 +0530

    Egg Dropping DP

---
+++ dynamic-programming/egg_dropping_DP.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int max(int a, int b) {
+    return a ^ ((a ^ b) & -(a < b));
+}
+
+int eggdroppingtrials(int eggs, int floors) {
+    int cache_trials[eggs+1][floors+1];
+
+    for (int i = 1; i <= floors; i++) {
+        cache_trials[1][i] = i;
+    }
+
+    for (int i = 1; i <= eggs; i++) {
+        cache_trials[i][1] = 1;
+        cache_trials[i][0] = 1;
+    }
+
+    for (int i = 2; i <= eggs; i++) {
+        for (int j = 2; j <= floors; j++) {
+            int res = 1 + max(cache_trials[i-1][j-1], cache_trials[i][floors-j]);
+            if (cache_trials[i][j] > res)
+                cache_trials[i][j] = res; 
+        }
+    }
+    return cache_trials[eggs][floors];
+}
+
+int main(int argc, char ** argv) {
+    int eggs;
+    cin>>eggs;
+
+    int floors;
+    cin>>floors;
+
+    cout << eggdroppingtrials(eggs, floors);
+}

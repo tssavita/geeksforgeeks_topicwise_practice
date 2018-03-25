0ac4e322a61faa32a8b1c238acf9b1a88a42c1d2
Savita Seetaraman <savita.seetaraman5@gmail.com>
2018-03-25 08:55:48 +0200

    greedy-algorithms-set-2-kruskals-minimum-spanning-tree-mst.cpp

--- greedy-algorithms/revision/greedy-algorithms-set-2-kruskals-minimum-spanning-tree-mst.cpp
+++ greedy-algorithms/revision/greedy-algorithms-set-2-kruskals-minimum-spanning-tree-mst.cpp
@@ -16,19 +16,21 @@ class ds {
     public:
         int parent;
         int rank;
-        ds find(ds *subsets, ds x) {
+        int find(ds *subsets, int x) {
             if (subsets[x].parent == x)
                 return subsets[x].parent;
             return find(subsets, subsets[x].parent);
         }
-        void unionds(ds x, ds y) {
-            if (x.rank > y.rank) {
-                y.parent = x; 
-                x.rank++;
+        void unionds(ds *subsets, int x1, int y1) {
+            int x = find(subsets, x1);
+            int y = find(subsets, y1);
+            if (subsets[x].rank > subsets[y].rank) {
+                subsets[y].parent = x; 
+                subsets[x].rank++;
             }
             else {
-                x.parent = y;
-                y.rank++;
+                subsets[x].parent = y;
+                subsets[y].rank++;
             }
         }
 };
@@ -40,29 +42,34 @@ class graph {
         int edge_num;
         vector<edge> edges;
         graph(int v, int e): vertex_num(v), edge_num(e) {}
-}
+};
 
 void mst(graph *g) {
-
-    int *subsets = new int[g->vertex_num - 1];
-    for (int j = 0; j < vertex_num; j++) 
 
     sort(g->edges.begin(), g->edges.end());
     int e = 0, i = 0;
 
-    edge order[];
+    vector<edge> order;
     int k = 0;
 
-    while ( e < g->vertex_num - 1) {
+    ds *subsets = new ds[g->vertex_num];
+    for (int i = 0; i < g->vertex_num; i++) {
+        subsets[i].rank = 0;
+        subsets[i].parent = i;
+    }
+
+    for (int e = 0; e < (g->vertex_num - 1); e++)
         edge next_edge = g->edges[i++];
         int x = find(subsets, next_edge.source);
         int y = find(subsets, next_edge.dest);
 
         if (x != y) {
-            order[k++] = edge;
+            order.push_back(edge);
             unionds(subsets, x, y);
         }
     }
+    for (int j = 0; j < k; j++) 
+        cout << order[j].source << " " << order[j].weight << " " << order[j].dest << endl;
 }
 
 int main(int argc, char ** argv) {

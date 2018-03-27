c24822c0d77318bac0c1812e269627e764ecc91c
Savita Seetaraman <savita.seetaraman5@gmail.com>
2018-03-27 07:55:57 +0200

    queue_string

---
+++ basic-concepts/queue_string.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+
+using namespace std;
+int main()
+{
+  queue<string> q;
+
+  cout << "Pushing one two three four\n";
+  q.push("one");
+  q.push("two");
+  q.push("three");
+  q.push("four");
+
+  cout << "Now, retrieve those values in FIFO order.\n";
+  while(!q.empty()) {
+    cout << "Popping ";
+    cout << q.front() << "\n";
+    q.pop();
+  }
+  cout << endl;
+
+
+  return 0;
+}

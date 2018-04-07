a47733b5f31d02bd2c05bfe80e9897adc3ea674b
Savita Seetaraman <savita.seetaraman5@gmail.com>
2018-04-07 23:42:13 +0200

    count-number-binary-strings-without-consecutive-1s.cpp

---
+++ dynamic-programming/count-number-binary-strings-without-consecutive-1s.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int consecutive(int n) {
+    int a[n], b[n];
+    a[0] = b[0] = 1; 
+    for (int i = 1; i < n; i++) { 
+        a[i] = a[i-1] + b[i-1];
+        b[i] = a[i-1];
+    }
+
+    return a[n-1] + b[n-1];
+}
+ 
+int main(int argc, char ** argv) {
+    int n;
+    cin>>n;
+
+    cout << consecutive(n);
+}
